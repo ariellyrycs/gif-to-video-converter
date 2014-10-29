7cf088a9e206f7913fe5b5094f36fa2782d2b829
ariellyrycs <ariel_556@hotmail.com>
2014-10-29 16:19:04 -0700

    animations

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -4,11 +4,29 @@
 /*globals $*/
 $(function () {
     'use strict';
+    var fileOnload = function(e) {
+        $('#insert-image').attr('src',e.target.result);
+        $('.fetch-url').removeClass('show-option');
+        $('.show-image').addClass('show-option');
+    };
+
+    $('#fileupload').change(function(e) {
+        var file = e.target.files[0],
+            imageType = /image.gif/,
+            reader = new FileReader();
+        console.log(file);
+        if (!file.type.match(imageType)){
+            return;
+        }
+        reader.onload = fileOnload;
+        reader.readAsDataURL(file);
+
+    });
     $('#send-gif').on('submit', function (e) {
         e.preventDefault();
-        console.dir(e.target[0].value);
     });
     $('#show-url-file').on('click', function () {
-        $('.fetch-url').addClass('show');
+        $('.show-image').removeClass('show-option');
+        $('.fetch-url').addClass('show-option');
     });
 });
